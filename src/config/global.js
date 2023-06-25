c372666085f35c0014a822e12952770766cc6b73
flizcanor <flizcanor@icloud.com>
2023-06-25 11:52:18 -0500

    Update global.js

--- src/config/global.js
+++ src/config/global.js
@@ -38,7 +38,7 @@ export default {
       },
       {
         nombreRuta: 'introduccion',
-        icono: 'fas fa-info',
+        icono: 'fas fa-info-circle',
         titulo: 'Introducción',
         desarrolloContenidos: true,
       },
